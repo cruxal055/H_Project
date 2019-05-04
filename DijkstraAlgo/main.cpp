5b4e2547fd37b8d775c0b4dc5d8050d63a19a376
Alan Lu <35384610+cruxal055@users.noreply.github.com>
2019-05-03 23:15:32 -0700

    Add files via upload
    update 5

--- DijkstraAlgo/main.cpp
+++ DijkstraAlgo/main.cpp
@@ -8,8 +8,12 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
    dijkstra temp;
    temp.perform("34.12416049793684", "-118.21936667211293", "where");
-//   temp.perform("34.112015756932813", "-118.201564947701129", "where");
-   temp.getShortestPath("34.112015756932813", "-118.201564947701129");
+
+ //   temp.perform("34.112015756932813", "-118.201564947701129", "where");
+
+       temp.getShortestPath("34.115197150389598", "-118.224690123437298");
+    //temp.getShortestPath("34.1317701627123", "-118.195737121438455");
+   temp.toString();
 
 
 
